28cd9e5c0aef7ac12f10112041500d64de159d96
Shahanas P <shahanas.ds24@duk.ac.in>
2025-05-22 16:33:29 +0530

    Update nltk_download.py

--- nltk_download.py
+++ nltk_download.py
@@ -1,5 +1,17 @@
 
+
 import nltk
+import os
+
+# Define a local download path
+NLTK_DATA_DIR = os.path.join(os.path.dirname(__file__), "nltk_data")
+
+# Add it to NLTK's path
+nltk.data.path.append(NLTK_DATA_DIR)
+
+# Download required models to the local path
+nltk.download("punkt", download_dir=NLTK_DATA_DIR)
+nltk.download("stopwords", download_dir=NLTK_DATA_DIR)
 nltk.download("punkt")
 nltk.download("stopwords")
 
